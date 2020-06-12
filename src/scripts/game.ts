0d06af852f1b9779ce6744890259772752fded49
tgulmine <tgulmine@gmail.com>
2020-06-12 20:06:33 -0300

    colyseus connect test

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,9 +1,29 @@
 import 'phaser'
 import MainScene from './scenes/mainScene'
 import PreloadScene from './scenes/preloadScene'
+import * as Colyseus from "colyseus.js";
+
+var client = new Colyseus.Client('ws://localhost:2567');
+
+const connect = async () => {
+  try {
+    const room = await client.joinOrCreate("game_room");
+    room.onStateChange(state => {
+      console.log(state);
+    })
+    console.log('joined room', room.sessionId);
+    room.send('pickCard', {name: 'Endorsi Jahad'})
+  } catch {
+    console.log('could not join room');
+  }  
+}
+
+connect();
 
 const DEFAULT_WIDTH = 1280
 const DEFAULT_HEIGHT = 720
+
+
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -26,5 +46,5 @@ const config: Phaser.Types.Core.GameConfig = {
 }
 
 window.addEventListener('load', () => {
-  const game = new Phaser.Game(config)
+  /* const game = new Phaser.Game(config) */
 })
